c9070b9eff377e82c5a120eab4d98a004d99e004
杜文丽 <1874980400@qq.com>
2020-10-30 09:36:04 +0800

    完成车辆自动根据数据中的数去接人和送人

--- scheduleWeb/src/App.vue
+++ scheduleWeb/src/App.vue
@@ -12,22 +12,28 @@
 
     <img src="./assets/people5.png" class="el-people5" id="divPeople5Id">
 
-    <img src="./assets/dest1.png" class="el-dest1" id = dest1Id>
+    <img src="./assets/dest1.png" class="el-dest1" id=dest1Id>
 
-    <img src="./assets/dest2.png" class="el-dest2" id = dest2Id>
+    <img src="./assets/dest2.png" class="el-dest2" id=dest2Id>
 
-    <img src="./assets/dest3.png" class="el-dest3" id = dest3Id>
+    <img src="./assets/dest3.png" class="el-dest3" id=dest3Id>
 
-    <img src="./assets/dest4.png" class="el-dest4" id = dest4Id>
+    <img src="./assets/dest4.png" class="el-dest4" id=dest4Id>
 
-    <img src="./assets/dest5.png" class="el-dest5" id = dest5Id>
+    <img src="./assets/dest5.png" class="el-dest5" id=dest5Id>
 
-    <h1>{{carCoordinate}}</h1>
-    <button class="el-button" @click="mySchedule(FIFOArr,'FIFO')">FIFO</button>
-    <button class="el-button" @click="mySchedule(SJFArr,'SJF')">SJF</button>
-    <button class="el-button" @click="mySchedule(EDFArr,'EDF')">EDF</button>
-    <button class="el-button" @click="mySchedule(PriorityArr,'Priority')">Priority</button>
-    <div id="main" style="width: 600px;height: 400px;"></div>
+    <div class="el-divIllustrate">
+
+      <button class="el-button" @click="mySchedule(FIFOArr,'FIFO')">FIFO</button>
+      <button class="el-button" @click="mySchedule(SJFArr,'SJF')">SJF</button>
+      <button class="el-button" @click="mySchedule(EDFArr,'EDF')">EDF</button>
+      <button class="el-button" @click="mySchedule(PriorityArr,'Priority')">Priority</button>
+      <h3>调度过程：{{peopleList}}</h3>
+      <h3>当前接送的人：{{currPeople}}</h3>
+      <h3>接送完成：{{finish}}</h3>
+      <div id="main" style="width: 600px;height: 400px;"></div>
+    </div>
+
   </div>
 </template>
 
@@ -41,7 +47,7 @@
         data() {
             return {
                 SJFArr: [2, 2, 1, 1, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-                FIFOArr: [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4,-1,-1,-1,-1,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-5,-5,-5,-5,-5],
+                FIFOArr: [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, -1, -1, -1, -1, -2, -2, -2, -2, -3, -3, -3, -3, -3, -3, -4, -4, -4, -4, -4, -5, -5, -5, -5, -5],
                 PriorityArr: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1],
                 EDFArr: [2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1],
                 carCoordinate: [20, 40],
@@ -50,11 +56,14 @@
                 people3: [200, 150],
                 people4: [50, 500],
                 people5: [1000, 450],
-                people1Destination:[500,50],
-                people2Destination:[100,500],
-                people3Destination:[1000,600],
-                people4Destination:[500,300],
-                people5Destination:[400,100],
+                people1Destination: [500, 50],
+                people2Destination: [100, 500],
+                people3Destination: [1000, 600],
+                people4Destination: [500, 300],
+                people5Destination: [400, 100],
+                peopleList: [],
+                currPeople: 1,
+                finish:[]
             }
         },
         methods: {
@@ -96,11 +105,13 @@
                 var that = this;
                 var timesRun = 0;
                 var interval = setInterval(function () {
-                    if (arr[timesRun] > 0){
-                        var currPeople = document.getElementById("divPeople"+arr[timesRun]+"Id");
-                        var currDest = document.getElementById("dest"+arr[timesRun]+"Id");
+                    if (arr[timesRun] > 0) {
+                        var currPeople = document.getElementById("divPeople" + arr[timesRun] + "Id");
+                        var currDest = document.getElementById("dest" + arr[timesRun] + "Id");
                         currPeople.style.visibility = 'visible';
                         currDest.style.visibility = 'visible';
+                        that.peopleList.push(arr[timesRun]);
+                        that.currPeople = arr[timesRun];
                     }
                     switch (arr[timesRun]) {
                         case 1:
@@ -144,8 +155,23 @@
                     carDiv.style.left = that.carCoordinate[0] + 'px';
                     carDiv.style.top = that.carCoordinate[1] + 'px';
                     if (timesRun >= arr.length) {
+                        var currPeople = document.getElementById("divPeople" + (arr[timesRun - 1] * (-1)) + "Id");
+                        var currDest = document.getElementById("dest" + (arr[timesRun - 1] * (-1)) + "Id");
+                        currPeople.style.visibility = 'hidden';
+                        currDest.style.visibility = 'hidden';
                         clearInterval(interval);
                     }
+                    if (timesRun - 1 >= 0 && arr[timesRun] != arr[timesRun - 1]){
+                        if (arr[timesRun - 1] < 0) {
+                            var currDest = document.getElementById("dest" + (arr[timesRun-1] * (-1)) + "Id");
+                            currDest.style.visibility = 'hidden';
+                            that.finish.push(arr[timesRun - 1]*(-1));
+                        }else {
+                            var currPeople = document.getElementById("divPeople" + (arr[timesRun-1]) + "Id");
+                            currPeople.style.visibility = 'hidden';
+                        }
+                    }
+
                     timesRun += 1;
 
                 }, 500);
@@ -198,17 +224,17 @@
 
   /* 更改element-UI按钮样式 */
   .el-div {
-    background-color: aquamarine;
+    /*background-color: aquamarine;*/
     width: 1590px;
     height: 600px;
   }
-
 
 
   .el-button {
     color: #fff;
     background-color: #3D9DD9;
     border-color: #5569B4;
+    position: relative;
   }
 
   .el-button:hover {
@@ -270,7 +296,8 @@
     left: 1000px;
     top: 450px;
   }
-  .el-dest1{
+
+  .el-dest1 {
     width: 100px;
     height: 100px;
     visibility: hidden;
@@ -278,7 +305,8 @@
     left: 500px;
     top: 50px;
   }
-  .el-dest2{
+
+  .el-dest2 {
     width: 100px;
     height: 100px;
     visibility: hidden;
@@ -286,7 +314,8 @@
     left: 100px;
     top: 500px;
   }
-  .el-dest3{
+
+  .el-dest3 {
     width: 100px;
     height: 100px;
     visibility: hidden;
@@ -294,7 +323,8 @@
     left: 1000px;
     top: 600px;
   }
-  .el-dest4{
+
+  .el-dest4 {
     width: 100px;
     height: 100px;
     visibility: hidden;
@@ -302,7 +332,8 @@
     left: 500px;
     top: 300px;
   }
-  .el-dest5{
+
+  .el-dest5 {
     width: 100px;
     height: 100px;
     visibility: hidden;
@@ -311,5 +342,12 @@
     top: 100px;
   }
 
-
+  .el-divIllustrate {
+    width: 400px;
+    height: 1000px;
+    position: absolute;
+    left: 1000px;
+    top: 20px;
+    /*background-color: #3D9DD9;*/
+  }
 </style>
